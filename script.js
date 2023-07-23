e5b989772c804cf5ee0f2cdb9271271b6e85f138
RamzesLys <105160616+RamzesLys@users.noreply.github.com>
2023-07-23 21:40:56 +0300

    add files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('mousemove', e => {
+  Object.assign(document.documentElement, {
+    style:`
+    --move-x: ${(e.clientX - window.innerWidth/2) * -.05}deg;
+    --move-y: ${(e.clientY - window.innerHeight/2) * -.1}deg;`
+  })
+})
